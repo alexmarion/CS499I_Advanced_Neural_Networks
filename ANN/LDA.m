64f0abe80ee640538c4cf0e21f080c485921d1f2
Matthew D'Amore <msd88@drexel.edu>
2017-04-17 12:33:31 -0400

    swiggity swooty

--- ANN/LDA.m
+++ ANN/LDA.m
@@ -1,10 +1,11 @@
-function [ reduced_fields ] = LDA( fields, percent_field_retention )
-    [num_classes, classes, fields] = load_image_data();
-    new_num_features = ceil(size(fields, 2) * percent_field_retention);
+function [ reduced_fields ] = LDA(classes, fields, percent_field_retention)
+    num_features = length(fields);
+    num_classes = length(unique(classes));
+    new_num_features = ceil(num_features * percent_field_retention);
     classified_fields = [classes, fields];
-    class_means = nan(num_classes, length(fields));
-    within_matrix = zeros(length(fields),length(fields));
-    between_matrix = zeros(length(fields),length(fields));
+    class_means = nan(num_classes, num_features);
+    within_matrix = zeros(num_features,num_features);
+    between_matrix = zeros(num_features,num_features);
     field_means = mean(fields);
 
     for i = 1:num_classes
@@ -12,10 +13,10 @@ function [ reduced_fields ] = LDA( fields, percent_field_retention )
         class_means(i,:) = mean(current_classes(:,2:end));
         class_pop = length(current_classes);
         within_matrix = within_matrix + (class_pop - 1) .* cov(current_classes(:,2:end));
-        between_matrix = class_pop .* (class_means(i,:) - field_means)' * (class_means(i,:) - field_means);
+        between_matrix = between_matrix + class_pop .* (class_means(i,:) - field_means)' * (class_means(i,:) - field_means);
     end
 
-    [e_vectors, ~] = eigs(within_matrix \ between_matrix, new_num_features);
+    [e_vectors, ~] = eigs(within_matrix \ between_matrix, 2);
     reduced_fields = fields * e_vectors;
 end
 
--- ANN/multi_class_ANN_param_tester.m
+++ ANN/multi_class_ANN_param_tester.m
@@ -1,31 +1,33 @@
 %% Accuracy Testing
-training_iters = 0:10:10000;
-accuracies = zeros(numel(training_iters),2);
-
-for i=1:numel(training_iters)
-    num_iters = training_iters(i);
-    [accuracy,training_error] = train_multi_class_ANN(true,false,true,false,false,20,num_iters);
-    accuracies(i,:) = [num_iters,accuracy];
-end
-
-figure();
-plot(accuracies(:,1), accuracies(:,2));
-legend('Training Accuracy');
-xlabel('Iterations');
-ylabel('Accuracy');
-
-%% Number of Hidden Nodes Testing
-% num_hidden_nodes = 20:20:1600;
-% accuracies = zeros(numel(num_hidden_nodes),2);
-% for i=1:numel(num_hidden_nodes)
-%     num_hidden = num_hidden_nodes(i);
-%     disp(num_hidden);
-%     [accuracy,training_error] = train_multi_class_ANN(true,false,true,false,false,num_hidden,1000);
-%     accuracies(i,:) = [num_hidden,accuracy];
+rng(0);
+% training_iters = 0:100:10000;
+% accuracies = zeros(numel(training_iters),2);
+% 
+% for i=1:numel(training_iters)
+%     num_iters = training_iters(i);
+%     disp(num_iters);
+%     [accuracy,training_error] = train_multi_class_ANN(true,false,true,false,false,20,num_iters);
+%     accuracies(i,:) = [num_iters,accuracy];
 % end
 % 
 % figure();
 % plot(accuracies(:,1), accuracies(:,2));
 % legend('Training Accuracy');
-% xlabel('Number of Hidden Nodes');
+% xlabel('Iterations');
 % ylabel('Accuracy');
+
+%% Number of Hidden Nodes Testing
+num_hidden_nodes = 20:20:1600;
+accuracies = zeros(numel(num_hidden_nodes),2);
+for i=1:numel(num_hidden_nodes)
+    num_hidden = num_hidden_nodes(i);
+    disp(num_hidden);
+    [accuracy,training_error] = train_multi_class_ANN(true,false,true,false,false,num_hidden,1000);
+    accuracies(i,:) = [num_hidden,accuracy];
+end
+
+figure();
+plot(accuracies(:,1), accuracies(:,2));
+legend('Training Accuracy');
+xlabel('Number of Hidden Nodes');
+ylabel('Accuracy');
