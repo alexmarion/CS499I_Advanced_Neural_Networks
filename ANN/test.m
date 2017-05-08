66935b5dc2ac757062981729422467b779f32c5f
Alex Marion <ajm482@drexel.edu>
2017-05-07 20:21:30 -0400

    Working on DNN

--- ANN/test.m
+++ ANN/test.m
@@ -5,8 +5,6 @@ image_size = 40;
 
 %% Parameters
 deep_ann.num_classes = 15;
-% deep_ann.num_hidden_node_layers = 3;
-% deep_ann.num_hidden_nodes = 1600;
 deep_ann.hidden_layer_division = 3;
 deep_ann.training_iters = 1000;
 deep_ann.eta = 0.5;
@@ -14,7 +12,7 @@ deep_ann.percent_field_retention = 0.95;
 
 %% Control Flow Values
 deep_ann.should_add_bias_to_input = false;
-deep_ann.should_add_bias_to_hidden = false;
+deep_ann.should_add_bias_to_hidden = true;
 deep_ann.should_std_data = true;
 deep_ann.should_perform_PCA = false;
 
@@ -28,9 +26,7 @@ num_testing_rows = length(testing_fields(:,1));
 
 num_data_cols = length(training_fields(1,:));
 
-%% Testing stuff
-% TODO: get rid of num_hidden_layer nodes and replace it with the number of
-% features
+%% Compute number of hidden layers based on input size and division ratio
 num_hidden_node_layers = 0;
 num_nodes_in_layer = num_data_cols;
 for i = 1:num_testing_rows
@@ -77,35 +73,41 @@ for i = 1:num_training_rows
 end
 
 %% Perform Forward/Backward Propagation with Batch Gradient Descent
-iter = 0;
-
 % Initialize weights as random    
 range = [-1,1];
 weights = cell(num_hidden_node_layers,1);
 new_layer_size = ceil(num_data_cols/deep_ann.hidden_layer_division);
 weights{1} = (range(2)-range(1)).*rand(num_data_cols, new_layer_size)+range(1);
+
 for layer = 2:num_hidden_node_layers - 1 
     new_layer_size = ceil(new_layer_size/deep_ann.hidden_layer_division);
     h = size(weights{layer-1},2);
     w = new_layer_size;
     weights{layer} = (range(2)-range(1)).*rand(h,w)+range(1);
 end
+
 h = size(weights{num_hidden_node_layers - 1},2);
 w = num_output_nodes;
 weights{num_hidden_node_layers} = (range(2)-range(1)).*rand(h, w)+range(1);
-%{
-beta = (range(2)-range(1)).*rand(num_data_cols, deep_ann.num_hidden_nodes) + range(1);
-theta = (range(2)-range(1)).*rand(deep_ann.num_hidden_nodes, num_output_nodes) + range(1);
 
+%{
 if deep_ann.should_add_bias_to_hidden
-    beta = [ones(num_data_cols,1) beta];
-    theta = [ones(1,num_output_nodes);theta];
+    for layer=1:num_hidden_node_layers
+        if mod(layer,2) == 0
+            weights{layer} = [ones(1,size(weights{layer},2));weights{layer}];
+        else
+            weights{layer} = [ones(size(weights{layer},1),1) weights{layer}];
+        end
+    end
 end
 %}
-%% Matrix to track training error for plotting
+% Matrix to track training error for plotting
 training_accuracy = zeros(deep_ann.training_iters, 2);
+
+% Track training h values
 training_h = cell(num_hidden_node_layers - 1,1);
 
+iter = 0;
 while iter < deep_ann.training_iters
     iter = iter + 1;    
     %% Forward Propagation
@@ -119,46 +121,22 @@ while iter < deep_ann.training_iters
     
     % Compute output layer
     training_o = activation_fxn(training_h{num_hidden_node_layers - 1} * weights{num_hidden_node_layers});
-    
-    % Compute hidden layer
-    % training_h = activation_fxn(std_training_fields * beta);
-
-    % Compute output layer    
-    % training_o = activation_fxn(training_h * theta);
 
     %% Backward Propagation
-    % Last layer
-    delta_output = new_training_classes - training_o;
-    weights{num_hidden_node_layers} = weights{num_hidden_node_layers} + ((deep_ann.eta/num_training_rows) * delta_output' * training_h{num_hidden_node_layers-1})';
+    deltas = cell(num_hidden_node_layers,1);
     
-    delta_hidden = (weights{num_hidden_node_layers} * delta_output')' .* (training_h{num_hidden_node_layers-1} .* (1 - training_h{num_hidden_node_layers-1}));
-    weights{num_hidden_node_layers-1} = weights{num_hidden_node_layers-1} + (deep_ann.eta/num_training_rows) * (delta_hidden' * training_h{num_hidden_node_layers-2})';
+    % Output layer delta
+    deltas{num_hidden_node_layers} = new_training_classes - training_o;
     
-    % Internal hidden layers
-    for layer=num_hidden_node_layers - 1:-1:3
-        delta_output = delta_hidden - training_h{layer};
-        weights{layer} = weights{layer} + ((deep_ann.eta/num_training_rows) * delta_output' * training_h{layer - 1})';
-        
-        delta_hidden = (weights{layer} * delta_output')' .* (training_h{layer-1} .* (1 - training_h{layer-1}));         
-        weights{layer-1} = weights{layer-1} + (deep_ann.eta/num_training_rows) * (delta_hidden' * training_h{layer-2})';
+    % Internal hidden layer deltas
+    for layer=num_hidden_node_layers-1:-1:1
+        deltas{layer} = (weights{layer + 1} * deltas{layer + 1}')' .* (training_h{layer} .* (1 - training_h{layer}));
     end
     
-    % First Layer
-    delta_output = delta_hidden - training_h{2};
-    weights{2} = weights{2} + ((deep_ann.eta/num_training_rows) * delta_output' * training_h{1})';
-
-    delta_hidden = (weights{2} * delta_output')' .* (training_h{1} .* (1 - training_h{1}));         
-    weights{1} = weights{1} + (deep_ann.eta/num_training_rows) * (delta_hidden' * std_training_fields)';
-    
-    % Compute output error
-    % delta_output = new_training_classes - training_o;
-    % Update theta
-    % theta = theta + ((deep_ann.eta/num_training_rows) * delta_output' * training_h)';
-    % Compute hidden error
-    % delta_hidden = (theta * delta_output')' .* (training_h .* (1 - training_h));
-    % Update beta
-    % beta = beta + (deep_ann.eta/num_training_rows) * (delta_hidden' * std_training_fields)';
-    
+    % Update weights
+    for layer=num_hidden_node_layers-1:-1:1
+        weights{layer+1} = weights{layer+1} + ((deep_ann.eta/num_training_rows) * deltas{layer+1}' * training_h{layer})';
+    end
     
     % Choose maximum output node as value
     [~,training_o] = max(training_o,[],2);
@@ -170,18 +148,23 @@ while iter < deep_ann.training_iters
 end
 
 %% Testing
-% Compute hidden layer
-%testing_h = activation_fxn(std_testing_fields * beta);
+% Compute first hidden layer
+testing_h = activation_fxn(std_testing_fields * weights{1});
+
+% Compute internal hidden layers
+for layer=2:num_hidden_node_layers - 1
+    testing_h = activation_fxn(testing_h * weights{layer});
+end
 
 % Compute output layer
-%testing_o = activation_fxn(testing_h * theta);
+testing_o = activation_fxn(testing_h * weights{num_hidden_node_layers});
 
 % Choose maximum output node as value
-%[~,testing_o] = max(testing_o,[],2);
+[~,testing_o] = max(testing_o,[],2);
 
 % Compute number of correct predictions
-%num_correct = numel(find(~(testing_classes - testing_o)));
-%testing_accuracy = num_correct/num_testing_rows;
+num_correct = numel(find(~(testing_classes - testing_o)));
+testing_accuracy = num_correct/num_testing_rows;
 
 % Plot the training error
 figure();
