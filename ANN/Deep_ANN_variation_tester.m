e90fc75acd4e51365b6817e00da291059ebd871d
Alex Marion <ajm482@drexel.edu>
2017-05-22 10:50:36 -0400

    added accuracies

--- ANN/Deep_ANN_variation_tester.m
+++ ANN/Deep_ANN_variation_tester.m
@@ -115,18 +115,18 @@ plot_training_accuracy(deep_ann,mean_testing_accuracy,[(1:1000)' mean_training_a
 
 %% YYNY
 deep_ann = set_control_flow_vals(deep_ann,[1,1,0,1]);
+%{
 rng(0);
 [num_classes,training_fields,training_classes,validation_fields,validation_classes,testing_fields,testing_classes] = load_and_shuffle_data(image_size);
 rng(0);
 [deep_ann,training_accuracy,validation_accuracy,testing_accuracy] = train_deep_ANN(deep_ann,training_fields,training_classes,validation_fields,validation_classes,testing_fields,testing_classes)
-%{
+%}
 rng(0);
 [~,s_training_accuracies,s_validation_accuracies,s_testing_accuracies] = cross_validate_deep_ANN(deep_ann,S,fields,classes);
 mean_testing_accuracy = mean(s_testing_accuracies(:,2));
 mean_training_accuracies = mean(s_training_accuracies(:,2:end));
 mean_validation_accuracy = mean(s_validation_accuracies(:,2));
 plot_training_accuracy(deep_ann,mean_testing_accuracy,[(1:1000)' mean_training_accuracies'],mean_validation_accuracy);
-%}
 %% YNYY
 deep_ann = set_control_flow_vals(deep_ann,[1,0,1,1]);
 rng(0);
